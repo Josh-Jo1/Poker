50df6fc734bf5b27fa5618b44443845dc6185a6d
Joshua Johnson <>
2022-02-05 12:47:08 -0500

    corrected card six to have value 6

--- card.h
+++ card.h
@@ -4,7 +4,7 @@
 #include <vector>
 
 // Types of ranks
-enum class Rank { Two = 2, Three = 3, Four = 4, Five = 5, Six = 5, Seven = 7,
+enum class Rank { Two = 2, Three = 3, Four = 4, Five = 5, Six = 6, Seven = 7,
                     Eight = 8, Nine = 9, Ten = 10, Jack = 11, Queen = 12, King = 13, Ace = 14 };
 // Vector of all ranks
 const std::vector<Rank> RANKS { Rank::Two, Rank::Three, Rank::Four, Rank::Five, Rank::Six, Rank::Seven,
